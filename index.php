0ab22a3afcdd2c3afaca65d5aac3242b7b1502cc
Michael Tolly <miketolly@gmail.com>
2017-02-28 15:29:57 -0600

    Hook up the new pages into a sequence

--- index.php
+++ index.php
@@ -16,9 +16,6 @@ function redirect_to($url) {
   die();
 }
 
-$loader = new Twig_Loader_Filesystem('templates/');
-$twig = new Twig_Environment($loader);
-
 function data_location($netid) {
   if (strpos($netid, '..') !== false) exit('Invalid email address.');
   return __DIR__ . '/data/' . $netid . '.json';
@@ -56,18 +53,67 @@ function save_user_data($data) {
   file_put_contents(data_location($_SESSION['netid']), json_encode($data));
 }
 
+$meeting_code = 'thecode';
+
+function can_access($page) {
+  global $user_data, $meeting_code;
+  switch ($page) {
+    case    'save-meeting': $level = 1; break;
+    case         'meeting': $level = 1; break;
+    case             'bio': $level = 2; break;
+    case        'save-bio': $level = 2; break;
+    case            'team': $level = 3; break;
+    case       'save-team': $level = 3; break;
+    case         'support': $level = 4; break;
+    case    'save-support': $level = 4; break;
+    case 'save-no-support': $level = 4; break;
+    case            'line': $level = 5; break;
+    case     'save-engage': $level = 5; break;
+    case  'save-transform': $level = 5; break;
+    case           'paper': $level = 6; break;
+    case      'save-paper': $level = 6; break;
+    default:             return true;
+  }
+  if ($level >= 1 && !array_key_exists('netid', $_SESSION)) return false;
+  if ($level >= 2 && $user_data['code'] !== $meeting_code) return false;
+  if ($level >= 3 &&
+    (  !array_key_exists('bio', $user_data)
+    || !array_key_exists('project-description', $user_data)
+    || !array_key_exists('project-title', $user_data)
+    )) return false;
+  if ($level >= 4 && !array_key_exists('team', $user_data)) return false;
+  if ($level >= 5 && !array_key_exists('support', $user_data)) return false;
+  if ($level >= 6 && !array_key_exists('line', $user_data)) return false;
+  return true;
+}
+
+function render_page($twig_name) {
+  global $user_data;
+  $loader = new Twig_Loader_Filesystem('templates/');
+  $twig = new Twig_Environment($loader);
+  echo $twig->render($twig_name, array(
+    'netid' => $_SESSION['netid'],
+    'data' => $user_data,
+    'paper_status' =>
+      (array_key_exists('submitted', $user_data) && $user_data['submitted'])
+      ? 'Submitted, review pending'
+      : 'Not submitted',
+  ));
+}
+
 $matched = true;
 if (count($parts) === 0) {
-  if (array_key_exists('netid', $_SESSION)) {
-    if (!array_key_exists('met-with-network', $user_data)) {
-      redirect_to('meeting');
-    } else {
-      redirect_to('paper');
-    }
-  } else {
-    echo $twig->render('login.twig', array());
-  }
+  if (can_access('paper')) redirect_to('paper');
+  if (can_access('line')) redirect_to('line');
+  if (can_access('support')) redirect_to('support');
+  if (can_access('team')) redirect_to('team');
+  if (can_access('bio')) redirect_to('bio');
+  if (can_access('meeting')) redirect_to('meeting');
+  render_page('login.twig');
 } else if (count($parts) === 1) {
+
+  if (!can_access($parts[0])) redirect_to('.');
+
   if ($parts[0] === 'logout') {
     unset($_SESSION['netid']);
     redirect_to('.');
@@ -75,60 +121,50 @@ if (count($parts) === 0) {
     $_SESSION['netid'] = $_POST['netid'];
     redirect_to('.');
   } else if ($parts[0] === 'meeting') {
-    if (!array_key_exists('netid', $_SESSION)) redirect_to('.');
-    echo $twig->render('meeting.twig', array(
-      'netid' => $_SESSION['netid'],
-      'data' => $user_data,
-    ));
+    render_page('meeting.twig');
   } else if ($parts[0] === 'save-meeting') {
-    if (!array_key_exists('netid', $_SESSION)) redirect_to('.');
     $user_data['code'] = $_POST['code'];
     save_user_data($user_data);
     redirect_to('.');
   } else if ($parts[0] === 'bio') {
-    if (!array_key_exists('netid', $_SESSION)) redirect_to('.');
-    echo $twig->render('bio.twig', array(
-      'netid' => $_SESSION['netid'],
-      'data' => $user_data,
-    ));
+    render_page('bio.twig');
+  } else if ($parts[0] === 'save-bio') {
+    $user_data['bio'] = $_POST['bio'];
+    $user_data['project-title'] = $_POST['project-title'];
+    $user_data['project-description'] = $_POST['project-description'];
+    save_user_data($user_data);
+    redirect_to('.');
   } else if ($parts[0] === 'project') {
-    if (!array_key_exists('netid', $_SESSION)) redirect_to('.');
-    echo $twig->render('project.twig', array(
-      'netid' => $_SESSION['netid'],
-      'data' => $user_data,
-    ));
+    render_page('project.twig');
   } else if ($parts[0] === 'team') {
-    if (!array_key_exists('netid', $_SESSION)) redirect_to('.');
-    echo $twig->render('team.twig', array(
-      'netid' => $_SESSION['netid'],
-      'data' => $user_data,
-    ));
+    render_page('team.twig');
   } else if ($parts[0] === 'save-team') {
-    if (!array_key_exists('netid', $_SESSION)) redirect_to('.');
     $user_data['team'] = json_decode($_POST['json']);
     save_user_data($user_data);
     redirect_to('.');
   } else if ($parts[0] === 'support') {
-    if (!array_key_exists('netid', $_SESSION)) redirect_to('.');
-    echo $twig->render('support.twig', array(
-      'netid' => $_SESSION['netid'],
-      'data' => $user_data,
-    ));
+    render_page('support.twig');
+  } else if ($parts[0] === 'save-support') {
+    $user_data['support'] = $_POST['support'];
+    save_user_data($user_data);
+    redirect_to('.');
+  } else if ($parts[0] === 'save-no-support') {
+    $user_data['support'] = false;
+    save_user_data($user_data);
+    redirect_to('.');
   } else if ($parts[0] === 'line') {
-    if (!array_key_exists('netid', $_SESSION)) redirect_to('.');
-    echo $twig->render('line.twig', array(
-      'netid' => $_SESSION['netid'],
-      'data' => $user_data,
-    ));
+    render_page('line.twig');
+  } else if ($parts[0] === 'save-engage') {
+    $user_data['line'] = 'engage';
+    save_user_data($user_data);
+    redirect_to('.');
+  } else if ($parts[0] === 'save-transform') {
+    $user_data['line'] = 'transform';
+    save_user_data($user_data);
+    redirect_to('.');
   } else if ($parts[0] === 'paper') {
-    if (!array_key_exists('netid', $_SESSION)) redirect_to('.');
-    echo $twig->render('paper.twig', array(
-      'netid' => $_SESSION['netid'],
-      'data' => $user_data,
-      'status' => $user_data['submitted'] ? 'Submitted, review pending' : 'Not submitted',
-    ));
+    render_page('paper.twig');
   } else if ($parts[0] === 'save-paper') {
-    if (!array_key_exists('netid', $_SESSION)) redirect_to('.');
     rename
       ( $_FILES['connect_project']['tmp_name']
       , paper_location($_SESSION['netid'], $_FILES['connect_project'])
@@ -137,10 +173,7 @@ if (count($parts) === 0) {
     save_user_data($user_data);
     redirect_to('.');
   } else if ($parts[0] === 'informed') {
-    echo $twig->render('informed.twig', array(
-      'netid' => $_SESSION['netid'],
-      'data' => $user_data,
-    ));
+    render_page('informed.twig');
   } else if ($parts[0] === 'submit-informed') {
     foreach (array('email', 'interests') as $k) {
       if (isset($_POST[$k])) {
@@ -150,10 +183,7 @@ if (count($parts) === 0) {
     file_put_contents(data_simple_location(), json_encode($user_data));
     redirect_to('thanks');
   } else if ($parts[0] === 'thanks') {
-    echo $twig->render('thanks.twig', array(
-      'netid' => $_SESSION['netid'],
-      'data' => $user_data,
-    ));
+    render_page('thanks.twig');
   } else if ($parts[0] === 'grand-challenges.csv') {
     $temp = tmpfile();
     fputcsv($temp, array('Email', 'Idea', 'Collaborators', 'Colleagues', 'Interest'));
